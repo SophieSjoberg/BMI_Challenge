833573f3f0d6396e90ac676db44f72152f14428f
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-06 15:41:36 +0100

    Add BMI calculator spec

---
+++ jasmine-standalone-2.8.0/spec/bmi_calculator_spec.js
@@ -0,0 +1,10 @@
+describe("BMICalculator", function() {
+  var calculator;
+  var person;
+
+beforeEach(function()  {
+  person = new Person({weight: 90, height: 186});
+  calculator = new BMICalculator();
+});
+
+}); 
