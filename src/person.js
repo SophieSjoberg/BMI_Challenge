f702d1213844ed5d7d4e50cee0169c7803583c01
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-08 16:08:13 +0100

    Modify so that tests for both metric and imperial pass

--- src/person.js
+++ src/person.js
@@ -7,6 +7,6 @@ Person.prototype.calculate_bmi = function() {
     this.bmiValue = 24.39;
     this.bmiMessage = "Healthy";
     calculator = new BMICalculator();
-    /*calculator.metric_bmi(this); */
+    calculator.metric_bmi(this);
     calculator.imperial_bmi(this);
   };
