24b04ca6b94c2d6ec687b7a2144830f75747bf74
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-08 10:22:14 +0100

    Add imperial method src to SpecRun and index.html

--- src/BMI_imperial_calculator.js
+++ src/BMI_imperial_calculator.js
@@ -10,3 +10,19 @@ BMICalculator.prototype.imperial_bmi = function(obj) {
       setBMIMessage(obj);
     }
 };
+
+function setBMIMessage(obj) {
+if (obj.bmiValue < 18.5) {
+  obj.bmiMessage = "Underweight"
+}
+if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
+  obj.bmiMessage = "Healthy"
+}
+if (obj.bmiValue > 25 && obj.bmiValue < 30) {
+  obj.bmiMessage = "Overweight"
+}
+if (obj.bmiValue > 30) {
+  obj.bmiMessage = "Obese"
+}
+
+}
