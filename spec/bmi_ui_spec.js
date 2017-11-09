de4d2724ba578225e5ad9e02f0217255d4d27720
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-09 10:15:26 +0100

    Modify tests for both metric and imperial functions

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -1,5 +1,7 @@
 
 describe('BMI_UI - index.html', function() {
+
+  describe("It calculates using metric system", function() {
     beforeEach(function() {
       jasmine.getFixtures().fixturesPath = '.';
       loadFixtures('index.html');
@@ -15,5 +17,26 @@ describe('BMI_UI - index.html', function() {
     it("displays BMI Message", function() {
       expect($('#display_message').text()).toBe('and you are Healthy');
     });
+});
+
+
+ describe("It calculates using imperial system", function() {
+
+ beforeEach(function() {
+   jasmine.getFixtures().fixturesPath = '.';
+   loadFixtures('index.html');
+   $.holdReady(false);
+   $('#weight').val('170');
+   $('#height').val('70');
+   $('#calculate').trigger('click');
+ });
+ it("displays BMI Value", function() {
+   expect($('#display_value').text()).toBe('Your BMI is 24.39');
+ });
+
+ it("displays BMI Message", function() {
+   expect($('#display_message').text()).toBe('and you are Healthy');
+ });
+});
 
 });
